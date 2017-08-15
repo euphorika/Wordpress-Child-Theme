27df02a73122378a6400c2293c4825886a0fe48c
Christopher Helmbold <christopher@markenbegeisterung.de>
2017-08-15 13:39:40 +0200

    Initialisation of Repository

---
+++ README.md
@@ -0,0 +1,15 @@
+Theme Name:     [PROJEKT]
+Author:         euphorika
+Author URI:     http://www.markenbegeisterung.de/
+Template:       Divi
+Version:        1.0
+
+** PROJEKT BESCHREIBUNG **
+
+
+** Vorrausstzungen **
+
+
+** RICHTLINIEN **
+
+- Bitte aktiv SASS-Funktionalitäten verwenden: http://thesassway.com
---
+++ functions.php
@@ -0,0 +1,20 @@
+<?php
+
+	define("MAIN_VERSION", 1.0);
+
+	add_action( 'wp_enqueue_scripts', 'load_parent_styles' );
+
+	function load_parent_styles() {
+    	wp_enqueue_style( 'parent-style', get_template_directory_uri() .'/style.css', array(), MAIN_VERSION );
+    	wp_enqueue_style( 'divi-style', get_stylesheet_uri() , array(), MAIN_VERSION );
+	}
+
+	/*CUSTOM SCRIPT WENN BENÖTIGT*/
+	// add_action( 'wp_enqueue_scripts', 'load_custom_script' );
+
+	// function load_custom_script(){
+	// 	wp_register_script( 'euphorika-script', get_stylesheet_directory_uri() . '/assets/javascript/script.js', array( 'jquery' ), MAIN_VERSION, true ,'all');
+ // 	  	wp_enqueue_script( 'euphorika-script');
+	// }
+
+?>
